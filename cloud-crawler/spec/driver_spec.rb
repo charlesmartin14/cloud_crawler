196cc83be730d1ec56c9c51a98aa2bd93093db40
Charles H Martin <charlesmartin14@gmail.com>
2013-03-20 14:12:19 -0700

    more specs done

--- cloud-crawler/spec/driver_spec.rb
+++ cloud-crawler/spec/driver_spec.rb
@@ -38,19 +38,12 @@ module CloudCrawler
 
       Driver.crawl(pages[0].url)
       run_jobs
-      @page_store.size.should == 4
-      
+      @page_store.size.should == 4  
     end
 
   end
 end
 
-#
-# core.should have(2).pages
-# core.pages.keys.should_not include(pages[1].url)
-# core.pages.keys.should_not include(pages[3].url)
-# end
-#
 # it "should be able to call a block on every page" do
 # pages = []
 # pages << FakePage.new('0', :links => ['1', '2'])
@@ -65,14 +58,8 @@ end
 # count.should == 3
 # end
 #
-# it "should not discard page bodies by default" do
-# CloudCrawler.crawl(FakePage.new('0').url, @opts).pages.values#.first.doc.should_not be_nil
-# end
-#
-# it "should optionally discard page bodies to conserve memory" do
-# # core = CloudCrawler.crawl(FakePage.new('0').url, @opts.merge({:discard_page_bodies => true}))
-# # core.pages.values.first.doc.should be_nil
-# end
+
+
 #
 # it "should provide a focus_crawl method to select the links on each page to follow" do
 # pages = []
@@ -87,93 +74,7 @@ end
 # core.should have(2).pages
 # core.pages.keys.should_not include(pages[1].url)
 # end
-#
-# it "should optionally delay between page requests" do
-# delay = 0.25
-#
-# pages = []
-# pages << FakePage.new('0', :links => '1')
-# pages << FakePage.new('1')
-#
-# start = Time.now
-# CloudCrawler.crawl(pages[0].url, @opts.merge({:delay => delay}))
-# finish = Time.now
-#
-# (finish - start).should satisfy {|t| t > delay * 2}
-# end
-#
-# it "should optionally obey the robots exclusion protocol" do
-# pages = []
-# pages << FakePage.new('0', :links => '1')
-# pages << FakePage.new('1')
-# pages << FakePage.new('robots.txt',
-# :body => "User-agent: *\nDisallow: /1",
-# :content_type => 'text/plain')
-#
-# core = CloudCrawler.crawl(pages[0].url, @opts.merge({:obey_robots_txt => true}))
-# urls = core.pages.keys
-#
-# urls.should include(pages[0].url)
-# urls.should_not include(pages[1].url)
-# end
-#
-# it "should be able to set cookies to send with HTTP requests" do
-# cookies = {:a => '1', :b => '2'}
-# core = CloudCrawler.crawl(FakePage.new('0').url) do |crawler|
-# crawler.cookies = cookies
-# end
-# core.opts[:cookies].should == cookies
-# end
-#
-# it "should freeze the options once the crawl begins" do
-# core = CloudCrawler.crawl(FakePage.new('0').url) do |crawler|
-# crawler.threads = 4
-# crawler.on_every_page do
-# lambda {crawler.threads = 2}.should raise_error
-# end
-# end
-# core.opts[:threads].should == 4
-# end
-#
-# describe "many pages" do
-# before(:each) do
-# @pages, size = [], 5
-#
-# size.times do |n|
-# # register this page with a link to the next page
-# link = (n + 1).to_s if n + 1 < size
-# @pages << FakePage.new(n.to_s, :links => Array(link))
-# end
-# end
-#
-# it "should track the page depth and referer" do
-# core = CloudCrawler.crawl(@pages[0].url, @opts)
-# previous_page = nil
-#
-# @pages.each_with_index do |page, i|
-# page = core.pages[page.url]
-# page.should be
-# page.depth.should == i
-#
-# if previous_page
-# page.referer.should == previous_page.url
-# else
-# page.referer.should be_nil
-# end
-# previous_page = page
-# end
-# end
-#
-# it "should optionally limit the depth of the crawl" do
-# core = CloudCrawler.crawl(@pages[0].url, @opts.merge({:depth_limit => 3}))
-# core.should have(4).pages
-# end
-# end
-#
-# end
-#
-#
-#
+##
 # describe "options" do
 # it "should accept options for the crawl" do
 # core = CloudCrawler.crawl(SPEC_DOMAIN, :verbose => false,
@@ -192,6 +93,8 @@ end
 # core.opts[:depth_limit].should == 3
 # end
 #
+
+# TODO:  re-implement this
 # it "should accept options via setter methods in the crawl block" do
 # core = CloudCrawler.crawl(SPEC_DOMAIN) do |a|
 # a.verbose = false
@@ -211,10 +114,4 @@ end
 # core.opts[:depth_limit].should == 3
 # end
 #
-# it "should use 1 thread if a delay is requested" do
-# CloudCrawler.crawl(SPEC_DOMAIN, :delay => 0.01, :threads => 2).opts[:threads].should == 1
-# end
-# end
-#
-# end
-# end
+
