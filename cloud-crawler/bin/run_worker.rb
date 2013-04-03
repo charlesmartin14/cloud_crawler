496f8298d2c1488b9dafc3cc31d3f690c2f41a3e
Charles H Martin <charlesmartin14@gmail.com>
2013-04-02 22:51:20 -0700

    changed default to 1 sec

--- cloud-crawler/bin/run_worker.rb
+++ cloud-crawler/bin/run_worker.rb
@@ -12,7 +12,7 @@ opts = Trollop::options do
   opt :qless_port,"",  :short => "-p", :default => 6379
   opt :qless_db, "", :short => "-d", :default => 0
   opt :qless_queue, "", :short => "-q", :default => "crawl"   # :multi => true
-  opt :interval, "", :short => "-i", :default => 5
+  opt :interval, "", :short => "-i", :default => 1
   opt :job_reserver, "", :short => "-r", :default => 'Ordered'
   opt :verbose, "", :short => "-v", :default => true
 end
